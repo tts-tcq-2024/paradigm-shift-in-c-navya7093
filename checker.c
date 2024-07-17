f4c15f48fefe3c2a2955b44d9755abbe5d153151
navya7093 <navyakontala@gmail.com>
2024-07-17 21:23:35 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,78 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
- 
-typedef int (*CheckFunc)(float);
- 
-typedef struct {
-    CheckFunc check;
-    float value;
-    const char *message;
-} Check;
- 
-void printMessage(const char *message) {
-    printf("%s", message);
-}
- 
-int isTemperatureInRange(float temperature) {
+
+int isTemperatureOk(float temperature) {
     return (temperature >= 0 && temperature <= 45);
 }
- 
-int isSocInRange(float soc) {
+
+int isSocOk(float soc) {
     return (soc >= 20 && soc <= 80);
 }
- 
-int isChargeRateInRange(float chargeRate) {
+
+int isChargeRateOk(float chargeRate) {
     return (chargeRate <= 0.8);
 }
- 
-int checkTemperature(float temperature) {
-    if (!isTemperatureInRange(temperature)) {
-        printMessage("Temperature out of range!\n");
-        return 0;
-    }
-    return 1;
+
+void printErrorMessage(const char* message) {
+    printf("%s\n", message);
 }
- 
-int checkSoc(float soc) {
-    if (!isSocInRange(soc)) {
-        printMessage("State of Charge out of range!\n");
-        return 0;
-    }
-    return 1;
-}
- 
-int checkChargeRate(float chargeRate) {
-    if (!isChargeRateInRange(chargeRate)) {
-        printMessage("Charge Rate out of range!\n");
-        return 0;
-    }
-    return 1;
-}
- 
-int performChecks(Check *checks, int numChecks) {
-    int allPassed = 1; // Assume all checks pass initially
-    for (int i = 0; i < numChecks; ++i) {
-        if (!checks[i].check(checks[i].value)) {
-            allPassed = 0; // Set to 0 if any check fails
-        }
-    }
-    return allPassed;
-}
- 
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    Check checks[] = {
-        {checkTemperature, temperature, NULL},
-        {checkSoc, soc, NULL},
-        {checkChargeRate, chargeRate, NULL}
-    };
-    return performChecks(checks, sizeof(checks) / sizeof(checks[0]));
+    if (!isTemperatureOk(temperature)) {
+        printErrorMessage("Temperature out of range!");
+        return 0;
+    }
+    if (!isSocOk(soc)) {
+        printErrorMessage("State of Charge out of range!");
+        return 0;
+    }
+    if (!isChargeRateOk(chargeRate)) {
+        printErrorMessage("Charge Rate out of range!");
+        return 0;
+    }
+    return 1;
 }
- 
+
 int main() {
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
-    assert(!batteryIsOk(30, 85, 0));
-    assert(!batteryIsOk(25, 70, 0.9));
-    printf("All tests passed!\n");
-    return 0;
 }
