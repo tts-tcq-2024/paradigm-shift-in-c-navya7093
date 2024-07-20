acd80fd0db90ed3a57870aa7c44bd823349fffbb
navya7093 <navyakontala@gmail.com>
2024-07-20 19:40:47 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,57 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
  
-#define TEMPERATURE_MIN 0
-#define TEMPERATURE_MAX 45
-#define SOC_MIN 20
-#define SOC_MAX 80
-#define CHARGE_RATE_MAX 0.8
- 
-typedef enum {
-    BATTERY_OK,
-    TEMPERATURE_ERROR,
-    SOC_ERROR,
-    CHARGE_RATE_ERROR
-} BatteryStatus;
- 
-void printErrorMessage(const char* message) {
-    printf("%s\n", message);
-}
- 
 int isTemperatureOk(float temperature) {
-    return (temperature >= TEMPERATURE_MIN && temperature <= TEMPERATURE_MAX);
+    if (temperature < 0 || temperature > 45) {
+        printf("Temperature out of range!\n");
+        return 0;
+    }
+    return 1;
 }
  
 int isSocOk(float soc) {
-    return (soc >= SOC_MIN && soc <= SOC_MAX);
+    if (soc < 20 || soc > 80) {
+        printf("State of Charge out of range!\n");
+        return 0;
+    }
+    return 1;
 }
  
 int isChargeRateOk(float chargeRate) {
-    return (chargeRate <= CHARGE_RATE_MAX);
+    if (chargeRate > 0.8) {
+        printf("Charge Rate out of range!\n");
+        return 0;
+    }
+    return 1;
 }
  
-int isError(BatteryStatus status) {
-    return status != BATTERY_OK;
-}
- 
-BatteryStatus checkBattery(float temperature, float soc, float chargeRate) {
-    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate) ? BATTERY_OK :
-           !isTemperatureOk(temperature) ? TEMPERATURE_ERROR :
-           !isSocOk(soc) ? SOC_ERROR :
-           CHARGE_RATE_ERROR;
-}
- 
-BatteryStatus updateBatteryStatus(BatteryStatus currentStatus, BatteryStatus newStatus) {
-    return isError(newStatus) ? newStatus : currentStatus;
-}
- 
-BatteryStatus batteryIsOk(float temperature, float soc, float chargeRate) {
-    return updateBatteryStatus(BATTERY_OK, checkBattery(temperature, soc, chargeRate));
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
  
 int main() {
-    assert(batteryIsOk(25, 70, 0.7) == BATTERY_OK);
-    assert(batteryIsOk(50, 85, 0) != BATTERY_OK);
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
     printf("All tests passed.\n");
     return 0;
 }
