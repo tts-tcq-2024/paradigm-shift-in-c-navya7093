5ea78b23faa1062240692c905e20b08dc0d81383
navya7093 <navyakontala@gmail.com>
2024-07-29 21:08:32 +0530

    Update warnings.c

--- warnings.c
+++ warnings.c
@@ -8,22 +8,40 @@ void enableWarnings(int enable) {
     warningsEnabled = enable;
 }
 
-void checkTemperatureWarning(float temperature) {
-    if (!warningsEnabled) return;
+void checkTemperatureWarningLow(float temperature) {
     if (temperature <= TEMP_MIN + TEMP_WARNING_TOLERANCE) {
         printf("Warning: Approaching discharge temperature\n");
-    } else if (temperature >= TEMP_MAX - TEMP_WARNING_TOLERANCE) {
+    }
+}
+
+void checkTemperatureWarningHigh(float temperature) {
+    if (temperature >= TEMP_MAX - TEMP_WARNING_TOLERANCE) {
         printf("Warning: Approaching peak temperature\n");
+    }
+}
+
+void checkTemperatureWarning(float temperature) {
+    if (!warningsEnabled) return;
+    checkTemperatureWarningLow(temperature);
+    checkTemperatureWarningHigh(temperature);
+}
+
+void checkSocWarningLow(float soc) {
+    if (soc <= SOC_MIN + SOC_WARNING_TOLERANCE) {
+        printf("Warning: Approaching discharge SoC\n");
+    }
+}
+
+void checkSocWarningHigh(float soc) {
+    if (soc >= SOC_MAX - SOC_WARNING_TOLERANCE) {
+        printf("Warning: Approaching peak SoC\n");
     }
 }
 
 void checkSocWarning(float soc) {
     if (!warningsEnabled) return;
-    if (soc <= SOC_MIN + SOC_WARNING_TOLERANCE) {
-        printf("Warning: Approaching discharge SoC\n");
-    } else if (soc >= SOC_MAX - SOC_WARNING_TOLERANCE) {
-        printf("Warning: Approaching peak SoC\n");
-    }
+    checkSocWarningLow(soc);
+    checkSocWarningHigh(soc);
 }
 
 void checkChargeRateWarning(float chargeRate) {
