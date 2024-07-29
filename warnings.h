1f55c3032ed3ccde27a81856b6c5f06574f69ca0
navya7093 <navyakontala@gmail.com>
2024-07-29 21:08:55 +0530

    Update warnings.h

--- warnings.h
+++ warnings.h
@@ -6,7 +6,11 @@
 #define CHARGE_RATE_WARNING_TOLERANCE (CHARGE_RATE_MAX * 0.05)
 
 void enableWarnings(int enable);
+void checkTemperatureWarningLow(float temperature);
+void checkTemperatureWarningHigh(float temperature);
 void checkTemperatureWarning(float temperature);
+void checkSocWarningLow(float soc);
+void checkSocWarningHigh(float soc);
 void checkSocWarning(float soc);
 void checkChargeRateWarning(float chargeRate);
 
