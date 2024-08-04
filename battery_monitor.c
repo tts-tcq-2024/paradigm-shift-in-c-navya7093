7d787b8630ca60a1870423007b6a8e692753b9ee
navya7093 <navyakontala@gmail.com>
2024-08-04 21:28:12 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -6,30 +6,41 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 }
 
 int isTemperatureOk(float temperature) {
-    checkEarlyWarning(temperature, 0, 45, 2.25, "Warning: Approaching lower temperature limit", "Warning: Approaching upper temperature limit");
+    checkTemperatureWarnings(temperature);
     return isValueInRange(temperature, 0, 45, "Temperature out of range!");
 }
 
 int isSocOk(float soc) {
-    checkEarlyWarning(soc, 20, 80, 4, "Warning: Approaching discharge", "Warning: Approaching charge-peak");
+    checkSocWarnings(soc);
     return isValueInRange(soc, 20, 80, "State of Charge out of range!");
 }
 
 int isChargeRateOk(float chargeRate) {
-    checkEarlyWarningForChargeRate(chargeRate, 0.8, 0.04, "Warning: Approaching maximum charge rate");
+    checkChargeRateWarnings(chargeRate);
     return isChargeRateInRange(chargeRate, 0.8, "Charge Rate out of range!");
 }
 
-void checkEarlyWarning(float value, float min, float max, float tolerance, const char* lowWarning, const char* highWarning) {
+void checkTemperatureWarnings(float temperature) {
+    checkLowWarning(temperature, 0, 2.25, "Warning: Approaching lower temperature limit");
+    checkHighWarning(temperature, 45, 2.25, "Warning: Approaching upper temperature limit");
+}
+
+void checkSocWarnings(float soc) {
+    checkLowWarning(soc, 20, 4, "Warning: Approaching discharge");
+    checkHighWarning(soc, 80, 4, "Warning: Approaching charge-peak");
+}
+
+void checkChargeRateWarnings(float chargeRate) {
+    checkHighWarning(chargeRate, 0.8, 0.04, "Warning: Approaching maximum charge rate");
+}
+
+void checkLowWarning(float value, float min, float tolerance, const char* lowWarning) {
     if (value >= min && value <= min + tolerance) {
         printf("%s\n", lowWarning);
     }
-    if (value >= max - tolerance && value <= max) {
-        printf("%s\n", highWarning);
-    }
 }
 
-void checkEarlyWarningForChargeRate(float value, float max, float tolerance, const char* highWarning) {
+void checkHighWarning(float value, float max, float tolerance, const char* highWarning) {
     if (value >= max - tolerance && value <= max) {
         printf("%s\n", highWarning);
     }
