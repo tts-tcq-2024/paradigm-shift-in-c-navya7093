9a0f514336ec8e2e9ed5f8d8f113ff2ef99f18a7
navya7093 <navyakontala@gmail.com>
2024-08-05 22:32:01 +0530

    Update Test_cases.c

--- Test_cases.c
+++ Test_cases.c
@@ -2,42 +2,43 @@
 #include "battery_monitor.h"
 #include "Test_cases.h"
 
-void testTemperatureBelowLowerLimit() {
+
+void TemperaturecheckBelowLowerLimit() {
     assert(!batteryIsOk(-1, 75, 0.6));
 }
 
-void testTemperatureAboveUpperLimit() {
+void TemperaturecheckAboveUpperLimit() {
     assert(!batteryIsOk(46, 75, 0.6));
 }
 
-void testSocBelowLowerLimit() {
+void SoccheckBelowLowerLimit() {
     assert(!batteryIsOk(30, 19, 0.6));
 }
 
-void testSocAboveUpperLimit() {
+void SoccheckAboveUpperLimit() {
     assert(!batteryIsOk(30, 81, 0.6));
 }
 
-void testChargeRateAboveUpperLimit() {
+void ChargeRatecheckAboveUpperLimit() {
     assert(!batteryIsOk(30, 75, 0.9));
 }
 
-void testApproachingLowerTemperatureLimit() {
+void ApproachingcheckLowerTemperatureLimit() {
     assert(batteryIsOk(2.25, 75, 0.6));
 }
 
-void testApproachingHigherTemperatureLimit() {
+void ApproachingcheckHigherTemperatureLimit() {
     assert(batteryIsOk(42.75, 75, 0.6));
 }
 
-void testApproachingDischarge() {
+void ApproachingDischargecheck() {
     assert(batteryIsOk(20, 24, 0.6));
 }
 
-void testApproachingChargePeak() {
+void ApproachingChargePeakcheck() {
     assert(batteryIsOk(20, 76, 0.6));
 }
 
-void testApproachingMaxChargeRate() {
+void ApproachingMaxChargeRatecheck() {
     assert(batteryIsOk(20, 40, 0.76));
 }
