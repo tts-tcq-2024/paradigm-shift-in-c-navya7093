02bb7a72ff1d1ea92857b30e336ce4935bfe5cc1
navya7093 <navyakontala@gmail.com>
2024-08-05 22:11:05 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -1,8 +1,58 @@
-#include <stdio.h>
-#include "battery_monitor.h"
+#include "Battery_Monitor.h"
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
+void printLowWarning(float value, float min, float tolerance, const char* lowWarning) {
+    if (value >= min && value <= min + tolerance) {
+        printf("%s\n", lowWarning);
+    }
+}
+
+void checkEarlyWarning(float value, float min, float max, float tolerance, const char* lowWarning, const char* highWarning) {
+    printLowWarning(value, min, tolerance, lowWarning);
+    printHighWarning(value, max, tolerance, highWarning);
+}
+
+
+
+void printHighWarning(float value, float max, float tolerance, const char* highWarning) {
+    if (value >= max - tolerance && value <= max) {
+        printf("%s\n", highWarning);
+    }
+}
+
+
+void checkEarlyWarningForChargeRate(float value, float max, float tolerance, const char* highWarning) {
+   printHighWarning(value, max, tolerance, highWarning);
+}
+
+
+
+void printWarningMessage(const char* WarningMessage) {
+    printf("%s\n", WarningMessage);
+}
+
+
+
+
+int isValueInRange(float value, float min, float max, const char* WarningMessage) {
+    if (value < min || value > max) {
+        printWarningMessage(WarningMessage);
+        return 0;
+    }
+    return 1;
+}
+
+int isChargeRateInRange(float value, float max, const char* WarningMessage) {
+    if (value > max) {
+        printWarningMessage(WarningMessage);
+        return 0;
+    }
+    return 1;
+}
+
+
+int isSocOk(float soc) {
+    checkEarlyWarning(soc, 20, 80, 4, "Warning: Approaching discharge", "Warning: Approaching charge-peak");
+    return isValueInRange(soc, 20, 80, "State of Charge out of range!");
 }
 
 int isTemperatureOk(float temperature) {
@@ -10,43 +60,15 @@ int isTemperatureOk(float temperature) {
     return isValueInRange(temperature, 0, 45, "Temperature out of range!");
 }
 
-int isSocOk(float soc) {
-    checkEarlyWarning(soc, 20, 80, 4, "Warning: Approaching discharge", "Warning: Approaching charge-peak");
-    return isValueInRange(soc, 20, 80, "State of Charge out of range!");
-}
+
 
 int isChargeRateOk(float chargeRate) {
     checkEarlyWarningForChargeRate(chargeRate, 0.8, 0.04, "Warning: Approaching maximum charge rate");
     return isChargeRateInRange(chargeRate, 0.8, "Charge Rate out of range!");
 }
 
-void checkEarlyWarning(float value, float min, float max, float tolerance, const char* lowWarning, const char* highWarning) {
-    if (value >= min && value <= min + tolerance) {
-        printf("%s\n", lowWarning);
-    }
-    if (value >= max - tolerance && value <= max) {
-        printf("%s\n", highWarning);
-    }
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
 
-void checkEarlyWarningForChargeRate(float value, float max, float tolerance, const char* highWarning) {
-    if (value >= max - tolerance && value <= max) {
-        printf("%s\n", highWarning);
-    }
-}
 
-int isValueInRange(float value, float min, float max, const char* warningMessage) {
-    if (value < min || value > max) {
-        printf("%s\n", warningMessage);
-        return 0;
-    }
-    return 1;
-}
-
-int isChargeRateInRange(float value, float max, const char* warningMessage) {
-    if (value > max) {
-        printf("%s\n", warningMessage);
-        return 0;
-    }
-    return 1;
-}
